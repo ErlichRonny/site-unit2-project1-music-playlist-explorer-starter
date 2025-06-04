e4ce5acc712b84368470e9df4bd8ee5bfd037fb2
Ronny Erlich <ronnye@meta.com>
2025-06-04 10:59:35 -0700

    Milestone 1 done - working on milestone 2

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,36 @@
+const modal = document.getElementById("playlistModal");
+const span = document.getElementsByClassName("close")[0];
 
+function openModal(playlistDict) {
+  document.getElementById("playlistTitle").innerText =
+    playlistDict.playlistTitle[0][0];
+  document.getElementById("creatorName").innerText =
+    playlistDict.playlistTitle[0][1];
+  document.getElementById("song1").innerText = playlistDict.playlistTitle[1][0];
+  document.getElementById("artist1").innerText =
+    playlistDict.playlistTitle[1][1];
+  document.getElementById("album1").innerText =
+    playlistDict.playlistTitle[1][2];
+  document.getElementById("song2").innerText = playlistDict.playlistTitle[2][0];
+  document.getElementById("artist2").innerText =
+    playlistDict.playlistTitle[2][1];
+  document.getElementById("album2").innerText =
+    playlistDict.playlistTitle[2][2];
+  document.getElementById("song2").innerText = playlistDict.playlistTitle[3][0];
+  document.getElementById("artist2").innerText =
+    playlistDict.playlistTitle[3][1];
+  document.getElementById("album2").innerText =
+    playlistDict.playlistTitle[3][2];
+
+  //     document.getElementById('albumName').innerText = playlist.albumName;
+  modal.style.display = "block";
+}
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
